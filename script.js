84a9429ff6dc2908f21c723adb023402b2855e02
AndreaDeGiorgi00 <andrea.adedegiorgi@gmail.com>
2023-01-09 16:24:38 +0100

    esercizio completato

---
+++ script.js
@@ -0,0 +1,13 @@
+const app = Vue.createApp({
+    data(){
+        return{
+        testo : "testo molto lungo che racconta le gesta di valorosi eroi in eroiche battaglie" ,
+        urlImmagine : "https://best5.it/b5/wp-content/uploads/2018/01/5-battaglie3-che-fecero-grande-lImpero-Romano-800x400.jpg"
+    
+    }}
+})
+
+
+//targhettizzo l'elemento 
+
+app.mount("#target")
